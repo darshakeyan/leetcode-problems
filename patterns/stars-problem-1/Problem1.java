31f53f90041a2cf65eb105433fb866fc5400615d
Darshak <darshak@Darshaks-MacBook-Pro.local>
2025-01-24 18:48:42 +0530

    stars and numbers pattern

---
+++ patterns/stars-problem-1/Problem1.java
@@ -0,0 +1,18 @@
+
+public class Problem1 {
+    public static void main(String[] args) {
+      // optimised solution 
+      int n = 5;
+      for (int i=0; i<=n; i++) {
+        System.out.println("* ".repeat(i));
+      }
+    }
+    public void loopSolution() {
+        for (int i=0; i<=5; i++) {
+          for (int j=0 ; j<i ; j++) {
+            System.out.print("*");
+          }
+          System.out.println();
+        }
+    }
+}
