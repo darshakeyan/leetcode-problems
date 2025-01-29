94f7c98fbbaa4b658454d9e4fab91b5cf0d4585c
Darshak <darshak@Darshaks-MacBook-Pro.local>
2025-01-29 12:34:54 +0530

    Star burst pattern

---
+++ patterns/hard/StarBurst.java
@@ -0,0 +1,40 @@
+package patterns.hard;
+
+public class StarBurst {
+    public static void main(String[] args) {
+        int rows = 5;
+        for(int i=1; i<=rows; i++) {
+          for (int j=1; j<=rows-i; j++) {
+           System.out.print(" "); 
+          }
+          for (int j=1; j<=(2*i)-1; j++) {
+            if (j == 1 || j == (2*i) - 1 || i == rows) {
+              System.out.print('*');
+            } else {
+              System.out.print(" ");
+            }
+          }
+          for (int j=1; j<=rows-i; j++) {
+           System.out.print(" "); 
+          }
+          System.out.println();
+        }
+        
+        for (int i=1; i<rows; i++) {
+          for (int j=1; j<=i; j++) {
+            System.out.print(' ');
+          }
+          for (int j=1; j<=(2*(rows-i) - 1); j++) {
+            if (j == 1 || j == 2*(rows-i) - 1 || i == rows) {
+              System.out.print('*');
+            } else {
+              System.out.print(" ");
+            }
+          }
+          for (int j=1; j<=i; j++) {
+            System.out.print(' ');
+          }
+          System.out.println();
+        }
+    }
+}
