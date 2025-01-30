ab74529af5fbc1366f7973ba2d25e0758ecad6d0
Darshak <darshak@Darshaks-MacBook-Pro.local>
2025-01-30 14:55:30 +0530

    problem 9 solved diamond

---
+++ patterns/strivers/ProblemNo9.java
@@ -0,0 +1,46 @@
+package patterns.strivers;
+
+public class ProblemNo9 {
+    public static void main(String[] args) {
+        int rows = 5;
+        for (int i=1; i<=rows; i++) {
+          for (int j=1; j<=rows-i; j++) {
+            System.out.print(" ");
+          }
+          for (int j=1; j<=(2*i)-1; j++) {
+            System.out.print("*");
+          }
+          for (int j=1; j<=rows-i; j++) {
+            System.out.print(" ");
+          }
+          System.out.println();
+        }
+        
+        for (int i=1; i<rows; i++) {
+          for (int j=1; j<=i; j++) {
+            System.out.print(' ');
+          }
+          for (int j=1; j<= (2*(rows-i)-1);j++) {
+            System.out.print("*");
+          }
+          System.out.println();
+        }
+    }
+}
+
+
+/*
+
+ Output:
+
+    *    
+   ***   
+  *****  
+ ******* 
+*********
+ *******
+  *****
+   ***
+    *
+
+ */
