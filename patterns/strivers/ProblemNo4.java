62b19ea8c754c4d9690b565f50dfaf2ff3835155
Darshak <darshak@Darshaks-MacBook-Pro.local>
2025-01-30 14:42:16 +0530

    problem 4 solved mostly similar 1 2 3 above

---
+++ patterns/strivers/ProblemNo4.java
@@ -0,0 +1,26 @@
+package patterns.strivers;
+
+public class ProblemNo4 {
+    public static void main(String[] args) {
+        int rows = 5;
+        for (int i=1; i<=rows; i++) {
+          for (int j=1; j<=i; j++) {
+            System.out.print(i);
+          }
+          System.out.println();
+        }
+    }
+}
+
+
+/* 
+
+Output:
+
+1
+22
+333
+4444
+55555
+
+ */
