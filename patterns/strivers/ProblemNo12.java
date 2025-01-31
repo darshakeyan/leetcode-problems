aff2049ed33e4c2ec7f3954561afab2dc12358a0
Darshak <darshak@Darshaks-MacBook-Pro.local>
2025-01-31 17:06:33 +0530

    problem 12 solved

--- patterns/strivers/ProblemNo12.java
+++ patterns/strivers/ProblemNo12.java
@@ -1,5 +1,28 @@
 package patterns.strivers;
 
 public class ProblemNo12 {
-    
+    public static void main(String[] args) {
+        int rows = 4;
+        for (int i=1; i<=rows; i++) {
+          for (int j=1; j<=i;j++) {
+            System.out.print(j);
+          }
+          for (int j=1; j<=(rows-i)*2; j++) {
+            System.out.print(' ');
+          }
+          for (int j=i; j>0 ;j--) {
+            System.out.print(j);
+          }
+          System.out.println();
+        }
+    }
 }
+
+/*
+
+1      1
+12    21
+123  321
+12344321
+
+ */
