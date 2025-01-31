822b1b33fa7960b48e3d2b762f858eb83a5f74e6
Darshak <darshak@Darshaks-MacBook-Pro.local>
2025-01-31 16:32:08 +0530

    problem 11 solved with help of striver

--- patterns/strivers/ProblemNo11.java
+++ patterns/strivers/ProblemNo11.java
@@ -1,5 +1,30 @@
 package patterns.strivers;
 
 public class ProblemNo11 {
-    
+    public static void main(String[] args) {
+        int rows = 5;
+        int start = 1;
+        for (int i=0; i<rows; i++) {
+          if (i % 2 == 0) start = 1;
+          else start = 0;
+          for (int j = 0; j<=i; j++) {
+            System.out.print(start);
+            start = 1 - start;
+          }
+          System.out.println();
+        }
+    }
 }
+
+
+/*
+
+Output:
+
+1
+01
+101
+0101
+10101
+
+ */
