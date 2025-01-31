158014cbb71c6207a0c22444a939d364766531dd
Darshak <darshak@Darshaks-MacBook-Pro.local>
2025-01-31 17:25:17 +0530

    using four loops problem 19 solved

--- patterns/strivers/ProblemNo19.java
+++ patterns/strivers/ProblemNo19.java
@@ -1,5 +1,56 @@
 package patterns.strivers;
 
 public class ProblemNo19 {
-    
+    public static void main(String[] args) {
+        int rows = 5;
+        for (int i=1; i<=rows; i++) {
+          for (int j=0; j<=rows-i ; j++) {
+            System.out.print("*");
+          }
+          for (int j=1; j<i; j++) {
+            System.out.print(" ");
+          }
+          for (int j=1; j<i; j++) {
+            System.out.print(" ");
+          }
+          for (int j=0; j<=rows-i; j++) {
+            System.out.print("*");
+          }
+          System.out.println();
+        }
+        
+       for (int i=1; i<=rows; i++) {
+        for (int j=0; j<i ; j++) {
+          System.out.print("*");
+        }
+        for (int j=1; j<=rows-i; j++) {
+          System.out.print(" ");
+        }
+        for (int j=1; j<=rows-i; j++) {
+          System.out.print(" ");
+        }
+        for (int j=0; j<i; j++) {
+          System.out.print("*");
+        }
+        System.out.println();
+      }
+    }
+
 }
+
+
+/*
+Output:
+
+**********
+****  ****
+***    ***
+**      **
+*        *
+*        *
+**      **
+***    ***
+****  ****
+**********
+
+ */
