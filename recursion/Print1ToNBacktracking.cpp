40e90515da49d8cd04921f7d32ae856d75018092
Darshak <darshak@Darshaks-MacBook-Pro.local>
2025-02-04 16:01:52 +0530

    printing n to 1 using forward and backward technique in recursion

--- recursion/Print1ToNBacktracking.cpp
+++ recursion/Print1ToNBacktracking.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 // backtracking recursion technique where it print and move ahead
 void printName(int i,int n) {
-    if (i<1) return;
+    if (i<1) return; // base condition where you have to stopped...
     printName(i-1,n);
     cout<<i<<endl;
 }
